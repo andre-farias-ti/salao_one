06914d26eaff7c2e24109a397170096a76e7cdbe
André Farias <andrefariasti@gmail.com>
2023-03-20 21:05:38 -0300

    primeiro commit

---
+++ src/main/java/br/com/salaoOne/salaoOne/SalaoOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.salaoOne.salaoOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/salaoOne/salaoOne/SalaoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.salaoOne.salaoOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaoOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
